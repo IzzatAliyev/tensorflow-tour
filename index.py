781878ac639c373c91194b39b5a322e4c3e4c420
Izzat Aliyev <izzat@izzats-mbp.speedport.ip>
2023-11-25 18:12:22 +0100

    added keras datasets download

--- index.py
+++ index.py
@@ -4,3 +4,10 @@ print("TensorFlow version:", tf.__version__)
 
 tensor = tf.constant("Hello, TensorFlow!")
 print(tensor)
+
+# download so lang and not efficient to build locally(better to use google colab)
+mnist = tf.keras.datasets.mnist
+print(mnist)
+
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+x_train, x_test = x_train / 255.0, x_test / 255.0
